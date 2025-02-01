0ca0676f70ae4503cdbd7240d76f2aa6669b9123
MohamedHamdanA <mdhamdan1111@gmail.com>
2025-02-01 16:17:32 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    switch (msg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, msg, wp, lp);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProcedure;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "SimpleWindow";
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(0, "SimpleWindow", "My First GUI", WS_OVERLAPPEDWINDOW, 
+                               CW_USEDEFAULT, CW_USEDEFAULT, 500, 400, nullptr, nullptr, hInstance, nullptr);
+
+    if (hwnd == nullptr) return 0;
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
